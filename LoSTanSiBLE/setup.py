1bacee79ed6213b59ca4387f45ac539fb7ac9f16
cdeck3r <cdecker@outlook.de>
2018-08-29 12:16:58 +0200

    Make tox useable in my docker-ized environment.

--- LoSTanSiBLE/setup.py
+++ LoSTanSiBLE/setup.py
@@ -1,9 +1,20 @@
 from setuptools import find_packages, setup
 
+#
+# you may want to read
+# https://packaging.python.org/discussions/install-requires-vs-requirements/
+# https://caremad.io/posts/2013/07/setup-vs-requirement/
+#
+
 setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
+    install_requires=[
+        "pandas",
+        "numpy",
+        "stockstats"
+    ],
     description='Low Speed Trading and Small in Budget; Large Expenses - A data science exercise project for stock market trading. ',
     author='Christian Decker',
     license='MIT',
